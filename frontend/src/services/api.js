48418439a4924451585158a08985ab3b15d365f8
Sabrina Li <chiawenli.1219@gmail.com>
2025-12-04 21:11:03 -0600

    base url render server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,14 +2,12 @@
 import axios from "axios";
 
 // [TODO]: Update with Gawon's LLM server
-const baseURL = "http://localhost:3001/api";
+// const baseURL = "http://localhost:3001/api";
+const baseURL = "https://cyberbullying-tuned.onrender.com/api";
 const instance = axios.create({
   baseURL: baseURL,
 });
 
 export const getLLMResponse = (data) => {
-  return instance.post(
-    `/getLLMResponse`,
-    data
-  );
+  return instance.post(`/getLLMResponse`, data);
 };
